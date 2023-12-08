bd5806bb27f625a00cd9c9b4a8d2d6cd9c924c7e
Julia <jdaianova@gmail.com>
2023-12-08 18:07:32 +0100

    6 commit

--- src/redux/ticketsFitersSlice.js
+++ src/redux/ticketsFitersSlice.js
@@ -1,28 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-// const initialState = {
-//   fromCityName: "",
-//   toCityName: "",
-//   fromCityId: "",
-//   toCityId: "",
-//   dateStart: "",
-//   dateEnd: "",
-//   haveWifi: null,
-//   isExpress: null,
-//   haveFirstClass: null,
-// };
-
 const initialState = {
-  fromCityName: "Москва",
-  toCityName: "Санкт-Петербург",
-  fromCityId: "641037eb5c49ea004632ee6e",
-  toCityId: "641037eb5c49ea004632ee6f",
-  dateStart: "2023-12-12",
-  dateEnd: "2023-12-18",
+  fromCityName: "",
+  toCityName: "",
+  fromCityId: "",
+  toCityId: "",
+  dateStart: "",
+  dateEnd: "",
   haveWifi: null,
   isExpress: null,
   haveFirstClass: null,
 };
+
+// const initialState = {
+//   fromCityName: "Москва",
+//   toCityName: "Санкт-Петербург",
+//   fromCityId: "641037eb5c49ea004632ee6e",
+//   toCityId: "641037eb5c49ea004632ee6f",
+//   dateStart: "2023-12-12",
+//   dateEnd: "2023-12-18",
+//   haveWifi: null,
+//   isExpress: null,
+//   haveFirstClass: null,
+// };
 
 export const ticketsFilterSlice = createSlice({
   name: 'filters',
