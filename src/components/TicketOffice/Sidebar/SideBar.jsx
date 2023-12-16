e2d34b799ba2263763697a2883c07cf5e24c49a1
Julia <jdaianova@gmail.com>
2023-12-17 00:16:12 +0100

    22 commit

--- src/components/TicketOffice/Sidebar/SideBar.jsx
+++ src/components/TicketOffice/Sidebar/SideBar.jsx
@@ -3,6 +3,7 @@ import './SideBar.css';
 import DateSection from './DateSection/DateSection';
 import PriceSlider from './PriceSlider/PriceSlider';
 import SwitchSection from './SwitchSection/SwitchSection';
+import TimeSlider from './TimeSlider/TimeSlider';
 
 const SideBar = () => {
 
@@ -11,6 +12,7 @@ const SideBar = () => {
       <DateSection/>
       <SwitchSection />
       <PriceSlider />
+      <TimeSlider />
     </aside>
   );
 };
---
+++ src/components/TicketOffice/Sidebar/TimeSlider/TimeSlider.jsx
@@ -0,0 +1,9 @@
+import './TimeSlider.css';
+
+const TimeSlider = () => {
+  return (
+    <div className='TimeSlider'>TimeSlider</div>
+  )
+}
+
+export default TimeSlider
